9cee1d658a0b40f790df67f629fb75ddd002495b
jdyuanglee <yuanglee39@gmail.com>
2018-07-17 19:30:08 -0400

    gitignore

--- wp-content/themes/demo/functions.php
+++ wp-content/themes/demo/functions.php
@@ -1,3 +1,7 @@
 <?php
-echo "functions";
+
+//Check users' WordPress version. 
+if (version_compare($GLOBALS['wp_version'], '4.9.6', '<')){
+    die('Your WordPress is so old. Please update to use this theme.');
+}
 ?>
