94bdeac75c8bb0346dd502e70bb5b9b0acd14f91
jdyuanglee <yuanglee39@gmail.com>
2018-07-17 20:13:51 -0400

    gitignore

--- wp-content/themes/demo/functions.php
+++ wp-content/themes/demo/functions.php
@@ -1,7 +1,16 @@
 <?php
 
-//Check users' WordPress version. 
+// Theme will only work for WordPress 4.9.6 or later 
 if (version_compare($GLOBALS['wp_version'], '4.9.6', '<')){
     die('Your WordPress is so old. Please update to use this theme.');
 }
+
+function register_menus(){
+    register_nav_menus([
+        'main_menu' => __('Main Navigation'),
+        'footer_menu' => __('Footer Navigation'),
+    ]);    
+}
+
+add_action('init', 'register_menus');
 ?>
