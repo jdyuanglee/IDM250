b91d94bf873fe96bb5f521f3be7447adb5255cd7
jdyuanglee <yuanglee39@gmail.com>
2018-09-08 10:54:31 -0400

    Add gerneral page

--- wp-content/themes/idm250/templates/template-general.php
+++ wp-content/themes/idm250/templates/template-general.php
@@ -1,21 +1,33 @@
 <?php
 /*
-Template Name: General Style Template
+Template Name: General Style Template Page
 */
 
 get_header(); ?>
 
-<main>
-    <h1 style="text-align:center ;"><?php the_title();?></h1>
+<main class="single-main">
+    <?php
+    $categoires = get_the_category();
+    ?>
+    <?php foreach($categoires as $category): ?>
+    <a href="<?php echo get_category_link($category); ?>"><?php echo $category -> name?></a>
+    <?php endforeach; ?>
+    </h3>
+    </div>
+    <?php   
+    if(has_post_thumbnail()): ?>
+    <?php the_post_thumbnail('full', ['class' => 'img-responsive']);?>
+    <?php else: ?>
+    <img src="https://via.placeholder.com/1280x720" alt="">
+    <?php endif;?>
     
     <div class="section">
-    <?php?>
-    <?php while (have_posts()) : the_post(); ?>
-        <?php the_content(); ?>
-    <?php endwhile;?>
-    <?php wp_reset_query();?>
+    <?php // TO SHOW THE PAGE CONTENTS?>
+    <?php while (have_posts()) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
+        <?php the_content(); ?> <!-- Page Content (WYSIWYG Field) -->
+    <?php endwhile; //resetting the page loop?>
+    <?php wp_reset_query(); //resetting the page query?>
     </div>
-    
 </main>
 
 <?php get_footer(); ?>
