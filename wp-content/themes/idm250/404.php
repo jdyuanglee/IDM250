7994b8372053c500d423c4b0500077056b6aa4d0
jdyuanglee <yuanglee39@gmail.com>
2018-09-08 10:50:12 -0400

    Add 404 page

--- wp-content/themes/idm250/404.php
+++ wp-content/themes/idm250/404.php
@@ -1,30 +1,9 @@
 <?php 
 /**
- * The main template file
+ * The template for displaying 404 pages (not found)
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
  */
 
-get_header(); ?>
-
-        <main>
-            <div class="searchbar">
-                <form class="search-container" action="404.html">
-                    <input type="text" id="search-bar" placeholder="Search...">
-                    <a href="404.html">
-                        <img class="search-icon" src="resources/magnify.png">
-                    </a>
-                </form>
-            </div>
-            <hr>
-            <div class="searchResult">
-                <p>Page not found. Please check your spelling and content.</p>
-            </div>
-        </main>
-
-<?php get_footer(); ?>
+get_template_part('templates/template-404');
---
+++ wp-content/themes/idm250/templates/template-404.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+Template Name: 404 Page
+*/
+get_header(); ?>
+
+<main>
+<div class="search">
+<main class="search-main">
+
+    <div class="searchResult">
+        <h1>404</h1>
+        <p>Page not found. Please check your spelling and content.</p>
+    </div>
+</main>
+</div>
+
+<?php get_footer(); ?>
