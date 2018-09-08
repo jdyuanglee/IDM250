86d173e73977d089b7f2e0b50df4e6254eac7d74
jdyuanglee <yuanglee39@gmail.com>
2018-09-08 10:52:15 -0400

    Add footer page

--- wp-content/themes/idm250/footer.php
+++ wp-content/themes/idm250/footer.php
@@ -1,31 +1,11 @@
 <div class="media">
-    <ul>
-        <li>
-            <a href="https://www.facebook.com/yuanglee">
-                <img src="dist/resources/fbIcon.svg" alt="fb">
-            </a>
-        </li>
-        <li>
-            <a href="https://www.snapchat.com/add/yuanglee39">
-                <img src="dist/resources/snapchatIcon.svg" alt="snapchat">
-            </a>
-        </li>
-        <li>
-            <a href="https://www.instagram.com/jdyuanglee/">
-                <img src="dist/resources/insIcon.svg" alt="twitter">
-            </a>
-        </li>
-        <li>
-            <a href="https://www.linkedin.com/in/yuang-li-18aa06125">
-                <img src="dist/resources/linkedinIcon.svg" alt="linkedin">
-            </a>
-        </li>
-        <li>
-            <a href="https://github.com/jdyuanglee">
-                <img src="dist/resources/ghIcon.svg" alt="github">
-            </a>
-        </li>
-    </ul>
+    <?php
+        wp_nav_menu([
+            'theme_location'  => 'footer',
+            'container'       => 'ul',
+            'menu_class'      => 'nav navbar-nav navbar-right',
+        ]);
+    ?>
 </div>
 
 <footer>
