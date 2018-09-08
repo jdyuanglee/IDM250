317d4158558fbc3d5a423021ff8ffc97f409fdd8
jdyuanglee <yuanglee39@gmail.com>
2018-09-08 10:53:10 -0400

    Add header.php

--- wp-content/themes/idm250/header.php
+++ wp-content/themes/idm250/header.php
@@ -7,70 +7,51 @@
 <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php the_title();?> | IDM250</title>
-
-<!-- Fontstyle -->
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-<link href="https://fonts.googleapis.com/css?family=Radley" rel="stylesheet">
-
-<!-- Javascript
-================================================== -->
-<script type="text/javascript" src="/dist/js/responsive_navbar.js"></script>
-
-<!-- Stylesethe
-================================================== -->
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/dist/css/general.css">
-<link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri(); ?>/dist/css/style.css">
-
 <!-- Open Wordpress Header Code -->
 <?php wp_head(); ?>
 <!-- Close Wordpress Header Code -->
-
 </head>
 
-<body <?php body_class('single');?>
-<header id="menutop"> 
-  
-  <!-- Navigation
-    ==========================================-->
-  <nav id="top-menu" class="navbar navbar-default navbar-fixed-top"> 
-    
+<body <?php body_class('single');?> >
+<header>
     <!--Header top-->
-    <div class="grid">
+    <div class="header">
 
         <div class="menu">
             <button class="hb-button">
-                <img src="dist/resources/threelines.png" alt="menu">
+                <img src="<?php echo get_template_directory_uri(); ?>/dist/resources/threelines.png" alt="menu">
             </button>
         </div>
         <div class="logo">
             <p>Yuang Li</p>
-            <img src="dist/resources/signature2.png" alt="logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/dist/resources/signature2.png" alt="logo">
         </div>
-        <div class="search" onclick="location.href='search.html';">
-            <a href="index.html">
-                <img src="dist/resources/cancel.png" alt="cross">
-            </a>
+        <div class="magnify">
+        <?php 
+          /**
+           * Will first attempt to locate the searchform.php file in either the child or the parent, then load it.
+           * If it doesn’t exist, then the default search form will be displayed.
+           * The default search form is HTML, which will be displayed.
+           * There is a filter applied to the search form HTML in order to edit or replace it. The filter is ‘get_search_form’.
+           * This will automatically go to /search. You can edit this template by creating a search.php.
+           *
+           * @link https://developer.wordpress.org/reference/functions/get_search_form/
+           *
+           */
+          get_search_form();
+          ?>       
         </div>
         <div class="ph"></div>
-
+    
         <nav>
-            <ul>
-                <li>
-                    <a href="index.html">about me</a>
-                </li>
-                <li>
-                    <a href="gallery.html">gallery</a>
-                </li>
-                <li>
-                    <a href="experience.html">experience</a>
-                </li>
-                <li>
-                    <a href="musing.html">musings</a>
-                </li>
-                <li>
-                    <a href="contact.html">contact</a>
-                </li>
-            </ul>
+        <?php
+            wp_nav_menu([
+                'theme_location'  => 'main',
+                'container'       => 'ul',
+                'menu_class'      => '',
+            ]);
+        ?>
         </nav>
+        
     <!--/Header top-->
 </header>
