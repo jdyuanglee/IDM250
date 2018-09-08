7ff5f99d875f0a6f5c242016cff0ec4a34ccabad
jdyuanglee <yuanglee39@gmail.com>
2018-09-08 10:54:37 -0400

    Add sidebar page

--- wp-content/themes/idm250/templates/template-single.php
+++ wp-content/themes/idm250/templates/template-single.php
@@ -1,39 +1,29 @@
 <?php
 /*
-Template Name: Single
+Template Name: Single Page
 */
 
 get_header(); ?>
 
-<main>
-    <div class="header">
-        <h1><?php the_title();?></h1>
-        <h3><?php the_title();?></h3>
-        <h3><?php the_date('m/d/Y');?></h3>
-        <h3>
-            <?php
-            $categoires = get_the_category();
-            ?>
-            <?php foreach($categoires as $category): ?>
-            <a href="<?php echo get_category_link($category); ?>"><?php echo $category -> name?></a>
-            <?php endforeach; ?>
-        </h3>
-    </div>
-    <?php   
-    if(has_post_thumbnail()): ?>
-    <?php the_post_thumbnail('full', ['class' => 'img-responsive']);?>
-    <?php else: ?>
-    <img src="https://via.placeholder.com/1280x720" alt="">
-    <?php endif;?>
-    
-    <div class="section">
+<main class="single-main">
+    <?php if (have_posts()): ?>
     <?php // TO SHOW THE PAGE CONTENTS?>
-    <?php while (have_posts()) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
-        <?php the_content(); ?> <!-- Page Content (WYSIWYG Field) -->
-    <?php endwhile; //resetting the page loop?>
-    <?php wp_reset_query(); //resetting the page query?>
+    <?php while (have_posts()) : the_post(); ?>
+    <div class="single-header">
+        <h1><?php the_title();?></h1>
+        <h3>Mobile Ordering App Design</h3>
+        <h3><?php the_date('m/d/Y');?></h3>
+        <h3><?php the_category( ' ' ); ?></h3>
     </div>
-    
+    <?php the_post_thumbnail('Portfolio')?>
+
+        <div class="single-section">
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile;?>
+    <?php wp_reset_query();?>
+    <?php else: ?>
+    <?php endif; ?>
 </main>
 
 <?php get_footer(); ?>
